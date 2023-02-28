9cb26ad24e1d259316044d8be65cf1f8d5fb0f9c
ur_sou1 <xorekvblendere@mail.ru>
2023-02-28 17:04:46 +1000

    123

--- MyCar.Web/wwwroot/js/site.js
+++ MyCar.Web/wwwroot/js/site.js
@@ -471,3 +471,23 @@ var Boxlayout = (function () {
 })();
 Boxlayout.init();    
 
+
+let wrapper = $(".tabs");
+let tabToggle = wrapper.find(".tab-toggle");
+function openTab() {
+    let content = $(this).parent().next(".Tabscontent"),
+        activeItems = wrapper.find(".active");
+    if ($(window).width() > 991) {
+        if (!$(this).hasClass('active')) {
+            $(this).add(content).add(activeItems).toggleClass('active');
+            wrapper.css('min-height', content.outerHeight());
+        }
+    } else {
+        $(this).add(content).toggleClass('active');
+    }
+};
+tabToggle.on('click', openTab);
+tabToggle.first().trigger('click');  
+
+
+

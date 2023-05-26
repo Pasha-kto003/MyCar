70264c55a16edaf7089231f5cff76167e196e55b
DeVice233 <yakovenko182003@gmail.com>
2023-05-26 21:04:29 +1000

    MarkPhoto

--- MyCar.Desktop/ViewModels/AddViewModels/AddMarkViewModel.cs
+++ MyCar.Desktop/ViewModels/AddViewModels/AddMarkViewModel.cs
@@ -18,6 +18,7 @@ namespace MyCar.Desktop.ViewModels.AddViewModels
 {
     public class AddMarkViewModel : BaseViewModel
     {
+
         private string searchText = "";
         public string SearchText
         {
@@ -72,24 +73,11 @@ namespace MyCar.Desktop.ViewModels.AddViewModels
             string dir = Environment.CurrentDirectory;
             AddImage = new CustomCommand(() =>
             {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.InitialDirectory = dir + @"\CarImages\";
-                if (openFileDialog.ShowDialog() == true)
+                MethodResult result = UIManager.AddImage("MarkImages");
+                if (result.IsSuccess)
                 {
-                    try
-                    {
-                        var info = new FileInfo(openFileDialog.FileName);
-                        var newPath = Environment.CurrentDirectory + @"\MarkImages\" + info.Name;
-                        if (!File.Exists(newPath))
-                            File.Copy(openFileDialog.FileName, newPath);
-                        ImageMark = info.Name;
-                        AddMark.MarkPhoto = info.Name;
-                    }
-
-                    catch (Exception e)
-                    {
-                        UIManager.ShowErrorMessage(new MessageBoxDialogViewModel { Message = e.Message });
-                    }
+                    ImageMark = result.Data.ToString();
+                    AddMark.MarkPhoto = result.Data.ToString();
                 }
             });
 
